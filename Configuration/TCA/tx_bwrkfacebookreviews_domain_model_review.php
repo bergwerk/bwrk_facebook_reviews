ddcc714b485137b804db1dcc435ff10543bca629
Georg Duemmler <gd@bergwerk.ag>
2016-05-03 16:32:14 +0200

    .

--- Configuration/TCA/tx_bwrkfacebookreviews_domain_model_review.php
+++ Configuration/TCA/tx_bwrkfacebookreviews_domain_model_review.php
@@ -2,17 +2,21 @@
 
 $configuration = new \BERGWERK\BwrkUtility\Utility\Tca\Configuration();
 $configuration->setExt('bwrk_facebook_reviews');
+$configuration->setModel('tx_bwrkfacebookreviews_domain_model_review');
+$configuration->setLabelField('user_name');
+$configuration->setIconFile('EXT:bwrk_facebook_reviews/ext_icon.png');
 
 $tca = new \BERGWERK\BwrkUtility\Utility\Tca\Tca();
 $tca->init($configuration);
 
-$tca->addCheckField('deleted');
+$tca->addPassThrough('deleted');
+$tca->addPassThrough('review_hash');
+$tca->addPassThrough('user_id');
+
 $tca->addCheckField('hidden');
-$tca->addInputField('review_hash');
-$tca->addInputField('created_time');
-$tca->addInputField('user_id');
-$tca->addInputField('user_name');
-$tca->addInputField('rating');
+$tca->addInputField('created_time', $tca->getFieldLabel('created_time'), 0, 30, 255, 1, 'trim');
+$tca->addInputField('user_name', $tca->getFieldLabel('user_name'), 0, 30, 255, 1, 'trim');
+$tca->addInputField('rating', $tca->getFieldLabel('rating'), 0, 30, 255, 1, 'trim');
 $tca->addTextField('review_text');
 
 return $tca->createTca();
